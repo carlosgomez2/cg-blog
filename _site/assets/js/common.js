3f3868270df15a3030099646cea7c62717086665
Carlos Gomez <carlosgomez.deb@gmail.com>
2018-01-11 15:10:45 -0600

    before upgrade npm packages, ALL WORKING

---
+++ _site/assets/js/common.js
@@ -0,0 +1,36 @@
+$(function() {
+	smoothScroll(700);
+  mobileNav();
+});
+
+function mobileNav() {
+	// Open Menu Mobile
+  $('.mobile-nav-toggle').on('click', function(){
+    var status = $(this).hasClass('is-open');
+    if(status){ $('.mobile-nav-toggle, .mobile-nav').removeClass('is-open'); }
+    else { $('.mobile-nav-toggle, .mobile-nav').addClass('is-open'); }
+  });
+
+	// Close Menu Mobile if link is clicked
+	$('.menu-link').click(function(){
+		$('#mobile-nav, .mobile-nav-toggle').toggleClass('is-open');
+	});
+
+}
+
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $( $(this).attr('href') );
+
+	    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, duration);
+				return false;
+	    }
+	});
+}
+
+// prueba 2 3 y 4
---
+++ _site/readme.md
@@ -0,0 +1,19 @@
+# Carlos Gómez personal Website Repo
+
+Hello folks!
+
+This is my main repo for my personal website.
+
+### Tools that I have used
+
+- Jekyll
+- Jade
+- Sass
+- Autoprefixer
+- Gulp
+- Bourbon
+- Browser-sync
+
+Fork from [Github: agragregra](https://github.com/agragregra/jekyll-gulp-autoprefixer-sass-bourbon-jade-browser-sync)
+
+Go to: [Site](https://carlosgomez2.github.io/cg-blog/)
